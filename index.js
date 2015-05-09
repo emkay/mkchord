2de5f4f1466cbfe3f8dc691a9130e09e102f04b2
Michael Matuzak <mmatuzak@gmail.com>
2015-05-09 14:50:16 -0700

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+var Note = require('octavian').Note;
+
+function Chord(n) {
+    this.root = new Note(n);
+}
+
+Chord.prototype.major = function major() {
+    return [this.root, this.root.majorThird(), this.root.perfectFifth()];
+};
+
+Chord.prototype.minor = function minor() {
+    return [this.root, this.root.minorThird(), this.root.perfectFifth()];
+};
